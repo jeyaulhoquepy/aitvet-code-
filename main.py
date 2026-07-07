def954a662402398e61beb56281934ab60242f0c
jeyaulhoquepy <jeyaulhoquepy@gmail.com>
2026-07-07 13:08:55 +0600

    the git

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hi")
+class trianglearea:
+    def __init__(self, a, b, c):
+        import math
+        if(a+b)>c and (b+c)>a and (a+c)>b:
+            s = (a + b + c) / 2
+            area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+            print("Area of triangle is:", area)
+        else:
+            print("Invalid triangle sides")
+a = float(input("Enter First Arm = "))
+b = float(input("Enter Second Arm = "))
+c = float(input("Enter Third Arm = "))
+triangle = trianglearea(a, b, c)
